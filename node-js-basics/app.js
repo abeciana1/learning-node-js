f3ca9fb8ae5a373f41155e6ee06b7c05411d2f87
Alex <alex.beciana@gmail.com>
2021-03-23 17:23:27 -0400

    Finished Node.js server setup with requests and responses

--- node-js-basics/app.js
+++ node-js-basics/app.js
@@ -1,9 +1,46 @@
 const http = require('http');
+const fs = require('fs');
 
 // console.log(http)
 
 const rqListener = (req, res) => { // req => request // res => response
-    console.log(req)
+    // console.log(req)
+    // console.log(req.url, req.method, req.headers)
+    // process.exit() //! Hard exit event loop for server
+    const url = req.url
+    const method = req.method
+    if (url === "/") {
+        res.write('<html>')
+        res.write('<body>')
+        res.write("<h2>Enter messages:</h2>")
+        res.write('<form action="/message" method="POST" ><input name="message" type="text"><button type="submit">Send</button></form>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            console.log(parsedBody)
+            const message = parsedBody.split("=")[1]
+            console.log(message)
+            fs.writeFileSync('message.txt', message)
+        })
+        res.writeHead(302, { 'Location': '/' })
+        return res.end()
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body>')
+    res.write('<h1>hello</h1>')
+    res.write('</body>')
+    res.write('</html>')
+    res.end()
 }
 
 const server = http.createServer(rqListener)
