fedc97a9fa133fcfa49e7dce0f3654b21188e3c6
Alex <alex.beciana@gmail.com>
2021-04-05 14:18:14 -0400

    Added catch-all middleware for 404 status code on undefined routes - Sends user to 404 Page Not Found page

--- express-js-parsing/app.js
+++ express-js-parsing/app.js
@@ -13,5 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(adminRoutes)
 app.use(shopRouters)
 
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page not found</h1>)
+}
+
 //! listening on port 3000
 app.listen(3000)
