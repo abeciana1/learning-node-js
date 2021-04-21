5582dcd91d33c38480ca9c19dd9c4873ca5bed96
Alex <alex.beciana@gmail.com>
2021-04-21 00:04:09 -0400

    Implemented use of ejs rendering and using partials

--- using-ejs/app.js
+++ using-ejs/app.js
@@ -14,10 +14,10 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(adminData.routes)
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(adminData.routes)
+app.use('/admin', adminData.routes)
 app.use(shopRouters)
 
 app.use((req, res, next) => {
--- using-ejs/routes/admin.js
+++ using-ejs/routes/admin.js
@@ -7,9 +7,12 @@ const router = express.Router()
 
 const products = []
 
-router.get("/add-product", (req, res, next) => {
+router.get("/admin/add-product", (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-  res.render('add-product', {pageTitle: "Add Product"})
+  res.render('add-product', {
+    pageTitle: "Add Product",
+    path: "/admin/add-product"
+  })
 });
 
 router.post("/product", (req, res, next) => {
